52fab8b96a071aeeabe5ea214bfed9912bbc9a67
Nguyễn Trung Hưng <43608238+dondi12345@users.noreply.github.com>
2021-07-14 09:27:11 +0700

    Add files via upload

---
+++ fe/FeAngular/src/app/model/type.ts
@@ -0,0 +1,12 @@
+
+import { Question } from "./question";
+
+export interface Type {
+    id: number 
+    create_date: string
+    update_date: string
+    delete_date: string
+    deleted: boolean
+    type_name: string 
+    questions: Question[]
+}
